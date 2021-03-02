aa36d528c199ef74173ce21c9371e1cd2a893d65
miagilling <70613238+miagilling@users.noreply.github.com>
2021-03-02 14:02:22 -0700

    updates

---
+++ stories-as-networks-final/script.js
@@ -0,0 +1,18 @@
+mybutton = document.getElementById("myBtn");
+
+// code from w3 schools
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 45000 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
