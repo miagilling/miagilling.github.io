514b34b6a2ac0d505515a76238384a81f5e85ce9
miagilling <70613238+miagilling@users.noreply.github.com>
2021-04-28 20:36:31 -0700

    update

--- input-output/javascript/script.js
+++ input-output/javascript/script.js
@@ -1,5 +1,29 @@
-let arrayA = ["apple", "anaconda", "all"];
-let arrayB = ["bubble", "bob"];
+let arrayA = ["ayesha", "aj", "adwoa"];
+let arrayB = ["black", "base", "becki"];
+let arrayC = ["clare","cheyenne", "cushnie"];
+let arrayD = ["dope", "defy"];
+let arrayE = ["eleven","ekua", "enda"];
+let arrayF = ["frolic", "fear", "fenty"];
+let arrayG = ["goodee", "gold", "glow"];
+let arrayH = ["harlem", "hyper", "helmer"];
+let arrayI = ["ivy", "iya", "island"];
+let arrayJ = ["johanna", "jam", "jacqs"];
+let arrayK = ["kendrick", "kosa", "kihry"];
+let arrayL = ["lapp", "luv"];
+let arrayM = ["mahogany", "mented", "maki"];
+let arrayN = ["nia", "nancy"];
+let arrayO = ["oat", "organic", "octave"];
+let arrayP = ["partake", "pat", "perry"];
+let arrayQ = ["queen"];
+let arrayR = ["roam", "rich", "red"];
+let arrayS = ["semicolon", "sankofa", "saint"];
+let arrayT = ["twelve", "theopolis", "thirteen"];
+let arrayU = ["uoma", "uncle", "uhai"];
+let arrayV = ["vavvuone","viz","virgo"];
+let arrayW = ["white", "wholistic"];
+let arrayX = ["xn", "xoxo"];
+let arrayY = ["yam", "yusef"];
+let arrayZ = ["zou", "zashadu", "zoezi"];
 
 document.eventListener("keydown", showBusiness);
 function showBusiness() {
