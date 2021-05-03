2d409fb145d71552b6047b1627c7e13126eff88e
miagilling <70613238+miagilling@users.noreply.github.com>
2021-05-02 22:56:45 -0700

    update

---
+++ tour/page-two/page-three/js/three.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("button");
+
+button.addEventListener("click",function(){
+	// document.body.style.backgroundColor = "black";
+	document.body.style.backgroundColor = "lightpink";});
