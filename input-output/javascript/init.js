fb44ec4ac1ab14caa456fba33aa23e2c1e9d6e10
miagilling <70613238+miagilling@users.noreply.github.com>
2021-05-10 18:23:28 -0700

    extra credit

--- input-output/javascript/init.js
+++ input-output/javascript/init.js
@@ -1,125 +1,48 @@
-// $(document).ready(function() {
-//  var entryCount = 0;
-//  var displayCount = 0;
-
-//   $(document).on("keypress", function(e) {
-//     e.preventDefault();
-//     entryCount ++;
-//     displayCount ++;
-       
-//     var char = String.fromCharCode(e.which);
-//     console.log("entry #" + entryCount + " : " + e.which + " | " + char + ". Showing " + displayCount);
-//     createElement(char);
-//   });
-
-//   $(document).on("keydown", function(e) {
-//     if (e.which == 8){
-//       e.preventDefault();
-//       entryCount ++;
-//       displayCount --;
-//       console.log("entry #" + entryCount + " : " + e.which + " | BKSP. Showing " + displayCount);
-//       deleteElement();
-//     }
-//   });
-
-// });
-
-// let arrayA = ["ayesha", "aj", "adwoa"];
-// let arrayB = ["black", "base", "becki"];
-// let arrayC = ["clare","cheyenne", "cushnie"];
-// let arrayD = ["dope", "defy"];
-// let arrayE = ["eleven","ekua", "enda"];
-// let arrayF = ["frolic", "fear", "fenty"];
-// let arrayG = ["goodee", "gold", "glow"];
-// let arrayH = ["harlem", "hyper", "helmer"];
-// let arrayI = ["ivy", "iya", "island"];
-// let arrayJ = ["johanna", "jam", "jacqs"];
-// let arrayK = ["kendrick", "kosa", "kihry"];
-// let arrayL = ["lapp", "luv"];
-// let arrayM = ["mahogany", "mented", "maki"];
-// let arrayN = ["nia", "nancy"];
-// let arrayO = ["oat", "organic", "octave"];
-// let arrayP = ["partake", "pat", "perry"];
-// let arrayQ = ["queen"];
-// let arrayR = ["roam", "rich", "red"];
-// let arrayS = ["semicolon", "sankofa", "saint"];
-// let arrayT = ["twelve", "theopolis", "thirteen"];
-// let arrayU = ["uoma", "uhai"];
-// let arrayV = ["vavvuone","virgo"];
-// let arrayW = ["white", "wholistic"];
-// let arrayX = ["xn", "xoxo"];
-// let arrayY = ["yam", "yusef"];
-// let arrayZ = ["zou", "zashadu", "zoezi"];
-
-// function createElement(k) {
-//   var elem = $('#cursor');
-//   if (k == "a" || k == "A") { elem.before(''); }
-//   if (k == "b" || k == "B") { elem.before('<span class="inner">B</span>'); }
-//   if (k == "c" || k == "C") { elem.before('<span class="inner"><img src="http://a.deviantart.net/avatars/i/n/infinitenyancatplz.gif"></span>'); }
-//   if (k == "d" || k == "D") { elem.before('<span class="inner">D</span>'); }
-//   if (k == "e" || k == "E") { elem.before('<span class="inner">E</span>'); }
-//   if (k == "f" || k == "F") { elem.before('<span class="inner">F</span>'); }
-//   if (k == "g" || k == "G") { elem.before('<span class="inner">G</span>'); }
-//   if (k == "h" || k == "H") { elem.before('<span class="inner">H</span>'); }
-//   if (k == "i" || k == "I") { elem.before('<span class="inner">I</span>'); }
-//   if (k == "j" || k == "J") { elem.before('<span class="inner">J</span>'); }
-//   if (k == "k" || k == "K") { elem.before('<span class="inner">K</span>'); }
-//   if (k == "l" || k == "L") { elem.before('<span class="inner">L</span>'); }
-//   if (k == "m" || k == "M") { elem.before('<span class="inner">M</span>'); }
-//   if (k == "n" || k == "N") { elem.before('<span class="inner">N</span>'); }
-//   if (k == "o" || k == "O") { elem.before('<span class="inner">O</span>'); }
-//   if (k == "p" || k == "P") { elem.before('<span class="inner">P</span>'); }
-//   if (k == "q" || k == "Q") { elem.before('<span class="inner">Q</span>'); }
-//   if (k == "r" || k == "R") { elem.before('<span class="inner">R</span>'); }
-//   if (k == "s" || k == "S") { elem.before('<span class="inner">S</span>'); }
-//   if (k == "t" || k == "T") { elem.before('<span class="inner">T</span>'); }
-//   if (k == "u" || k == "U") { elem.before('<span class="inner">U</span>'); }
-//   if (k == "v" || k == "V") { elem.before('<span class="inner">V</span>'); }
-//   if (k == "w" || k == "W") { elem.before('<span class="inner">W</span>'); }
-//   if (k == "x" || k == "X") { elem.before('<span class="inner">X</span>'); }
-//   if (k == "y" || k == "Y") { elem.before('<span class="inner">Y</span>'); }
-//   if (k == "z" || k == "Z") { elem.before('<span class="inner">Z</span>'); }
-//   if (k == " ") { elem.before('<span class="inner">&nbsp;</span>') };
-// }
-
-// function deleteElement() {
-//   $(".inner").last().remove();
-// }
-
-
-
+// this function ensures that all of my divs don't show up on the page at once
 function hideAllElements()
 {
+  // the query selector grabs all of my divs that have the class name business
   let allElements = document.querySelectorAll(".business");
   allElements.forEach(function(business) {
+    // and makes sure that if their display style is set to block,
     if (business.style.display === "block") {
+      // that it does not show up
       business.style.display = "none";
     }
   })
 }
 
+// each of these event listeners listens for a specific letter key to be pressed down
 document.addEventListener("keydown", function(){
   var x=event.keyCode || event.which;
+  // this first one is for the letter a, and its keycode is 65
   if(x==65)
   {
   hideAllElements();
+  // when the a key is pressed, my div with the id adwoa will show up 
   var y = document.getElementById("adwoa");
+  // its display style is initially set to none
   if (y.style.display === "none")
   {
+    // but after you press the a key, the display style will change to block
   document.getElementById("adwoa").style.display = "block";
   }
   else
   {
+  // if the a key isn't pressed, it will continue to have its display style set to none
   document.getElementById("adwoa").style.display = "none";
   }
   }
   })  
 
+  // this same process is repeated for each key
 document.addEventListener("keydown", function(){
   var x=event.keyCode || event.which;
+  // this is where i change the key code to correspond with each letter key
   if(x==66)
   {
   hideAllElements();
+  // and this is where i grab the div that corresponds with that key
   var y = document.getElementById("black");
   if (y.style.display === "none")
   {
